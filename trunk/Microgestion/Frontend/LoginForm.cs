7ee38b8e4b3bc8d6692042fc97de88016d99855a
javier.feldman <javier.feldman@b44b3e68-f6a1-11dd-a7be-4b3ef3b0700c>
2009-02-09 19:04:21 +0000

    Added Frontend project.

---
+++ trunk/Microgestion/Frontend/LoginForm.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Blackspot.Microgestion.Frontend
+{
+    public partial class LoginForm : DevExpress.XtraEditors.XtraForm
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
